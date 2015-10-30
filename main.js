acb70a14b4db53ee94e1c1d87ad2cc322dec916b
stevennoble78 <stevennoble78@hotmail.com>
2015-10-30 00:10:59 -0700

    add more animations, fix bugs

--- main.js
+++ main.js
@@ -2,38 +2,65 @@ $(document).ready(function(){
 
 	console.log("Sanity Check: JS is working!");
 
-	$('a').on('click', function (event) {
-		$(this).css('font-style', 'oblique');
-		$(this).css('color', 'red');
-		event.preventDefault();
-	});
-
-	$('p').on('mouseover', function (event) {
-		$(this).css('font-size', 24);
-	});
-
-	$('li').on('mouseover', function (event) {
-		$(this).css('font-size', 24);
-	});
-
-	$('img').on('click', function (event) {
-		$(this).fadeOut(6000);
-	});
+	alert("WARNING:  NOT FOR THE FAINT OF HEART.  If you are prone to seizures, please navigate away from this window.");
 
 	function getRand(num1, num2) {
 		return Math.round((num1 + (num2 - num1) * Math.random()));
 	}
 
-	var colorArray = ['lightpink',
+	var colorArray = ['pink',
+										'salmon', 
+										'yellow', 
+										'green', 
+										'blue', 
+										'coral'];
+
+	$('a').on('click', function (event) {
+		color = colorArray[getRand(0,5)];
+		$(this).css('font-style', 'oblique');
+		$(this).css('color', color);
+		event.preventDefault();
+		$(this).blink();
+	});
+
+	$('p').on('mouseover', function (event) {
+		color = colorArray[getRand(0,5)];
+		$(this).css('color', color);
+		$(this).css('font-size', 24);
+	});
+
+	$('li').on('mouseover', function (event) {
+		color = colorArray[getRand(0,5)];
+		$(this).css('color', color);
+		$(this).css('font-size', 24);
+	});
+
+	$('h3').on('click', function (event) {
+		$(this).fadeOut(6000);
+	});
+
+
+	$('img').on('click', function(event) {
+		$(this).animate({
+	    opacity: 'toggle',
+ 	  }, 12000, function() {
+		});
+	});
+
+
+	var bkgColArray = ['lightpink',
 										'lightsalmon', 
 										'lightyellow', 
 										'lightgreen', 
 										'lightblue', 
 										'lightcoral'];
 
-
 	$(window).on('scroll', function (event) {
-		color = colorArray[getRand(0, 5)];
+		color = bkgColArray[getRand(0, 5)];
+		$('.panel-body').css('background-color', color);
+		color = bkgColArray[getRand(0, 5)];
+		$('.img-embedded').css('background-color', color);
+		color = bkgColArray[getRand(0, 5)];
 		$('body').css('background-color', color);
 		console.log("Background Check!!");
 	});	
